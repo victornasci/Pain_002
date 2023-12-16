73d81152b8629387e3856744c58fd144a4877f2c
victornasci <vrn150@gmail.com>
2023-12-16 14:23:46 -0300

    aa

--- pain-vite/routes/RoutesMain.jsx
+++ pain-vite/routes/RoutesMain.jsx
@@ -1,31 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../src/pages/HomePage";
 import { AboutPage } from "../src/pages/AboutPage";
-import { CardsPage } from "../src/pages/CardsPage";
 
 export const RoutesMain = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    // Recarregar a página ao mudar de rota
-    const handleNavigation = () => {
-      window.location.reload();
-    };
-
-    // Adicionar um ouvinte para a mudança de rota
-    navigate(handleNavigation);
-
-    // Remover o ouvinte quando o componente for desmontado
-    return () => {
-      navigate(handleNavigation);
-    };
-  }, [navigate]);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/sobre-nos" element={<AboutPage />}></Route>
-      <Route path="/cards" element={<CardsPage />}></Route>
     </Routes>
   );
 };
